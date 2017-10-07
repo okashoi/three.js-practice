fa864de93a20c6ea3f0a0d8ebf5fddbd8d1e86e0
Okada Shohei <okashoicircus0@gmail.com>
2017-10-07 18:56:30 +0900

    a core sphere and a stellite

--- index.js
+++ index.js
@@ -1,43 +1,54 @@
 window.addEventListener('DOMContentLoaded', init);
 
+var Core = function () {
+
+};
+
 function init() {
-    var screenWidth = 800;
-    var screenHeight = 600;
+    var screenWidth = window.innerWidth;
+    var screenHeight = window.innerHeight;
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(screenWidth, screenHeight);
     document.body.appendChild(renderer.domElement);
 
-    var angleOfView = 45;
+    var angleOfView = 75;
     var aspectRatio = screenWidth / screenHeight;
-    var nearClip = 1;
-    var farClip = 10000;
+    var nearClip = 10;
+    var farClip = 200;
     var camera = new THREE.PerspectiveCamera(angleOfView, aspectRatio, nearClip, farClip);
-    camera.position.set(0, 0, 1000);
-
+    camera.position.set(0, 0, 110);
 
     var scene = new THREE.Scene();
 
-    var cubeWidth = 500;
-    var cubeHeight = 500;
-    var cubeDepth = 500;
-    var cubeGeometry = new THREE.BoxGeometry(cubeWidth, cubeHeight, cubeDepth);
-    var cubeMaterial = new THREE.MeshPhongMaterial({ color: 0x0080ff });
-    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    scene.add(cube);
+    var coreSphereRadius = 10;
+    var coreSphereGeometry = new THREE.SphereGeometry(coreSphereRadius);
+    var coreSphereMaterial = new THREE.MeshLambertMaterial({color: 0xc0c0c0});
+    var coreSphere = new THREE.Mesh(coreSphereGeometry, coreSphereMaterial);
+    scene.add(coreSphere);
+
+    var satelliteSphereRadius = 1;
+    var satelliteSphereGeometry = new THREE.SphereGeometry(satelliteSphereRadius);
+    var satelliteSphereMaterial = new THREE.MeshLambertMaterial({color: 0xc0c0c0});
+    var satelliteSphere = new THREE.Mesh(satelliteSphereGeometry, satelliteSphereMaterial);
+    scene.add(satelliteSphere);
+    satelliteSphere.position.x = 15;
 
     var light = new THREE.DirectionalLight(0xffffff);
     scene.add(light);
-    light.position.set(1, 1, 1);
+    light.position.set(0, 0, 200);
+    light.target = coreSphere;
 
+    var theta = 0;
 
     tick();
-
     function tick() {
         requestAnimationFrame(tick);
 
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
+        theta += Math.PI / 90;
+        theta = theta % (2 * Math.PI);
+        satelliteSphere.position.x = 15 * Math.cos(theta);
+        satelliteSphere.position.z = 15 * Math.sin(theta);
 
         renderer.render(scene, camera);
     }
