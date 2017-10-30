bb33354d2857793ee35bc9245714b153cc539bfe
Okada Shohei <okashoicircus0@gmail.com>
2017-10-31 02:50:30 +0900

    create a tower

--- index.js
+++ index.js
@@ -1,44 +1,112 @@
 window.addEventListener('DOMContentLoaded', init);
 
+var renderer, scene, camera;
+var states = {theta: 0};
+
+var Particle = function (orbitalRadius, offsetY, offsetTheta) {
+    this.material = new THREE.MeshLambertMaterial({color: 0x888888});
+    this.geometry = new THREE.SphereGeometry(1, 1, 1);
+    this.orbitalRadius = orbitalRadius;
+    this.offsetTheta = offsetTheta;
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.mesh.position.y = offsetY;
+};
+
+Particle.prototype.updatePosition = function () {
+    var theta = states.theta + this.offsetTheta;
+    this.mesh.position.x = this.orbitalRadius * Math.cos(theta);
+    this.mesh.position.z = this.orbitalRadius * Math.sin(theta);
+};
+
+var Ring = function (radius, particlesCount, offsetY, offsetTheta) {
+    this.particles = [];
+    for (var i = 0; i < particlesCount; i++) {
+        var theta = 2 * Math.PI / particlesCount * i + offsetTheta;
+        this.particles.push(new Particle(radius, offsetY, theta));
+    }
+};
+
+Ring.prototype.update = function () {
+    this.particles.map(function (particle) {
+        particle.updatePosition();
+    });
+};
+
+var Tower = function (radius, floorsCount, ringParticlesCount, floorYDistance, floorThetaDistance) {
+    this.rings = [];
+    var height = floorYDistance * (floorsCount - 1);
+    for (var i = 0; i < floorsCount; i++) {
+        var y = floorYDistance * i - height / 2;
+        var theta = floorThetaDistance * i;
+        this.rings.push(new Ring(radius, ringParticlesCount, y, theta));
+    }
+};
+
+Tower.prototype.update = function () {
+    this.rings.map(function (ring) {
+        ring.update();
+    });
+};
+
 function init() {
-    var screenWidth = 800;
-    var screenHeight = 600;
+    //--------------------------
+    // animation configurations
+    //--------------------------
+    // screen
+    var screenWidth = window.innerWidth;
+    var screenHeight = window.innerHeight;
+    // camera
+    var angleOfView = 75;
+    var nearClip = 100;
+    var farClip = 210;
 
-    var renderer = new THREE.WebGLRenderer();
+    // objects
+    // tower
+    var radius = 300;
+    var floorsCount = 30;
+    var ringParticlesCount = 20;
+    var floorYDistance = 10;
+    var floorThetaDistance = Math.PI / 7;
+
+    var angularVelocity = Math.PI / 1080;
+
+    //-------
+    // setup
+    //-------
+    // renderer
+    renderer = new THREE.WebGLRenderer();
     renderer.setSize(screenWidth, screenHeight);
-    document.body.appendChild(renderer.domElement);
-
-    var angleOfView = 45;
+    renderer.setClearColor(0xffffff, 1.0);
+    document.getElementsByClassName('render-area')[0].appendChild(renderer.domElement);
+    // scene
+    scene = new THREE.Scene();
+    // camera
     var aspectRatio = screenWidth / screenHeight;
-    var nearClip = 1;
-    var farClip = 10000;
-    var camera = new THREE.PerspectiveCamera(angleOfView, aspectRatio, nearClip, farClip);
-    camera.position.set(0, 0, 1000);
+    camera = new THREE.PerspectiveCamera(angleOfView, aspectRatio, nearClip, farClip);
+    camera.position.set(0, 0, -100);
 
 
-    var scene = new THREE.Scene();
+    //----------------
+    // put 3D objects
+    //----------------
+    var tower = new Tower(radius, floorsCount, ringParticlesCount, floorYDistance, floorThetaDistance);
+    tower.rings.map(function (ring) {
+        ring.particles.map(function (particle) {
+            scene.add(particle.mesh);
+        });
+    });
 
-    var cubeWidth = 500;
-    var cubeHeight = 500;
-    var cubeDepth = 500;
-    var cubeGeometry = new THREE.BoxGeometry(cubeWidth, cubeHeight, cubeDepth);
-    var cubeMaterial = new THREE.MeshPhongMaterial({ color: 0x0080ff });
-    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    scene.add(cube);
+    draw();
+    function draw() {
+        requestAnimationFrame(draw);
 
-    var light = new THREE.DirectionalLight(0xffffff);
-    scene.add(light);
-    light.position.set(1, 1, 1);
+        // update global states
+        states.theta = (states.theta + angularVelocity) % (2 * Math.PI);
 
+        // update 3D objects states
+        tower.update();
 
-    tick();
-
-    function tick() {
-        requestAnimationFrame(tick);
-
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-
+        // re-render
         renderer.render(scene, camera);
     }
 }
